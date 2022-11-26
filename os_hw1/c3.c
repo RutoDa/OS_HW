1832b3a0e10d3a2387d4e2450a1b43081d15e0ee
UULi <denny99203@gmail.com>
2022-11-26 16:24:19 +0800

    update2

---
+++ os_hw1/c3.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/* #include <conio.h> */
+
+int kbhit(void){
+struct timeval tv; fd_set read_fd;
+
+/* Do not wait at all, not even a microsecond */
+tv.tv_sec=0;
+tv.tv_usec=0;
+
+/* Must be done first to initialize read_fd */
+FD_ZERO(&read_fd);
+
+/* Makes select() ask if input is ready: 0 is the file descriptor for stdin */
+FD_SET(0, &read_fd);
+
+/* The first parameter is the number of the largest file descriptor to check + 1. */
+if (select(1, &read_fd, NULL /*No writes*/, NULL /*No exceptions*/, &tv) == -1)
+return 0; /* An error occured */
+
+/* read_fd now holds a bit map of files that are readable.
+* We test the entry for the standard input (file 0). */
+if (FD_ISSET(0, &read_fd)) /* Character pending on stdin */
+return 1;
+
+/* no characters were pending */
+return 0;
+} 
+
+int main() {
+    char c = 'a';
+    int i = 1;
+    pid_t pid = getpid();
+    while (1) {
+      sleep(3);
+      printf("I am the child. pid: %d\n", pid);
+      printf("%d Continue? Type s to quit.\n", i++);
+      if (kbhit()) {
+        c = getchar();
+        if (c == 's') exit(0);
+        printf("Typed %c.\n", c);
+      }
+    }
+}
---
+++ os_hw1/guess.c
@@ -0,0 +1,73 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int count(char* buffer)
+{
+    int count=0;
+    //do some stuff here, come on this should be easy guys!
+    return count;
+}
+ 
+int main(int argc, char **argv)
+{
+  int a, b, i, j;
+  char c;
+  char answer[5], guess[256];
+
+  printf("my pid is %d\n", getpid());
+  printf("(%s) Guess a 4-digit number. Type \'q\' to quit.\n", argv[0]);
+  while (1) {
+    srand( (unsigned) time(0));
+    while (1) {
+	answer[0] = '0' + (rand()%5);
+	answer[1] = '0' + (rand()%5);
+	answer[2] = '0' + (rand()%5);
+	answer[3] = '0' + (rand()%5);
+	if (answer[0] != answer[1] && answer[0] != answer[2] && answer[0] != answer[3] && answer[1] != answer[2] && answer[1] != answer[3] && answer[2] != answer[3]) break;
+    } // make answer
+    answer[4] = '\0';
+    //    printf(" ans %s\n", answer);
+    while(1)
+        {
+                //print the prompt
+	  printf("(%s) Guess: ", argv[0]);
+                //get input
+                fscanf(stdin, "%s", guess);
+		guess[4] = '\0';
+		//		printf(" input %s\n", guess);
+		if (guess[0] == 10) continue;
+		if (guess[0] == 'q') break;
+		if (guess[0] < '0' || guess[0] > '9' || guess[1] < '0' || guess[1] > '9' || guess[2] < '0' || guess[2] > '9' || guess[3] < '0' || guess[3] > '9')
+		  {
+		    printf("invalid number. try again\n");
+		    continue;
+		  }
+		a = b =0;
+		for (i = 0; i < 4; i++) {
+		  if (answer[i] == guess[i]) a++;
+		  for (j = 0; j < 4; j++) {
+		    if (i == j) continue;
+		    if (guess[i] == answer[j]) {
+		      b++;
+		      break;
+		    }
+		  }
+		}
+		printf(" %d A, %d B\n", a, b);
+		if (a == 4) {
+		  printf("Bingo. Continue (Y/N)? ");
+		  break; // leave guessing for this term
+		}
+		//		  if (c == 'Y')
+		//}
+        } // end this term
+    if (guess[0] == 'q') break;
+    while ( (c = fgetc(stdin)) != 'Y' && c != 'y' && c != 'N' && c!= 'n') ;
+    if (c == 'Y' || c == 'y') continue;
+    else break;
+  }
+  return 0;
+}
