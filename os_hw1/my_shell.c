23fd5f4d3b11e573f4161ad8e2c95d345c3a933d
UULi <denny99203@gmail.com>
2022-12-03 11:39:05 +0800

    Update my_shell

--- os_hw1/my_shell.c
+++ os_hw1/my_shell.c
@@ -8,13 +8,10 @@
 void input_split(char *input_str, char *path)  {
     // 用迴圈方式將 input string 中的路徑 存放到 path
     for (int i = 0; i < strlen(input_str); i++) {
-        if (input_str[i] == ' ') { 
-            // 假如讀到空格表示 input string 的 path 已結束
+        if (input_str[i] == ' ' || input_str[i] == '&'  || input_str[i] == '\n' ) { 
+            // 假如讀到空格或\n或&表示 input string 的 path 已結束
             path[i] = '\0'; // 將 path 最後一格填上 \0 變為字串
             break;
-        } else if (input_str[i] == '\n') { 
-            // 假如讀到 \n 表示 input string 整個讀完了
-            path[i] = '\0'; // 將 path 最後一格填上 \0 變為字串
         } else {
             // 將 path index i 的 element 填上 input 的 i 的 element
             path[i] = input_str[i];
@@ -25,7 +22,6 @@ void input_split(char *input_str, char *path)  {
 int main () {
     char no_wait = 0;
     pid_t parent_pid = getpid();
-    pid_t pid;
     printf("Parent pid is %d\n", parent_pid);
     for(;;) {
         char path[1025]; // store path，linux 路徑最長限制為 1024byte + 字串最後一格填 \0
@@ -46,27 +42,36 @@ int main () {
         
             if (no_wait == 1) {
                 // my_shell 不會 wait child
-                pid = fork(); // 產生child
+                pid_t pid = fork(); // 產生child
                 if (pid == 0) {
                     // child 
                     execl(path,path,NULL); // 將 child 改成指定路徑的程式碼
                     printf("%s is not found. fork() succeeded\n", path);
                     exit(1); // execl 失敗就會中止 child
-                } 
+                } else if (pid > 0) {
+                    // parent
+                } else {
+                    // fork() failed
+                    printf("fork() failed\n");
+                    exit(1);
+                }
             } else {
                 // my_shell 會 wait child
-                pid = fork(); // 產生child
+                pid_t pid = fork(); // 產生child
 
                 if (pid == 0) {
-                    // parent 會進來
+                    // chlild
                     execl(path,path,NULL);
                     printf("%s is not found. fork() succeeded\n", path);
-                    exit(1); // execl 失敗就會中止 child
+                    exit(777); // execl 失敗就會中止 child ， 並回傳 777
                 } else if (pid > 0) {
-                    pid_t ch = wait(&child_exit_status);
-                    printf("\nI'm parent. waitpid %d, status %d\n", pid, child_exit_status);
+                    // parent
+                    wait(&child_exit_status);
+                    if (child_exit_status != 777) // 777 表示 child 沒成功更換，不需執行以下輸出
+                        printf("\nI'm parent. waitpid %d, status %d\n", pid, child_exit_status);
                 } else {
-                    printf("%s is not found. fork() succeeded\n", path);
+                    // fork() failed
+                    printf("fork() failed\n");
                     exit(1);
                 }
             }   
